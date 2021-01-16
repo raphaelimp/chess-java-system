0cafbc5b3e239b206db9f00b7025f9f281f25538
Raphael Imperio <imperioraphael@gmail.com>
2021-01-16 17:37:20 -0300

    BoarException and defense programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+public class BoardException extends RuntimeException {
+
+	public BoardException(String msg) {
+		super(msg);
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
