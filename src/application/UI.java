72214af9f17a8afc31439fd9987f00404001132e
Raphael Imperio <imperioraphael@gmail.com>
2021-01-18 08:24:12 -0300

    Possible moves of King

--- src/application/UI.java
+++ src/application/UI.java
@@ -62,7 +62,7 @@ public class UI {
 		for(int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for(int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j], possibleMoves[i][j]);
+				printPiece(pieces[i][j], possibleMoves[i][j]);	
 			}
 			System.out.println();
 		}
